2142748c129d21a665d883ea0c39a8c4b2e4aa2a
刘思鑫 <fordreamxkhl@163.com>
2020-04-20 00:18:05 +0800

    refactor: 抽离公共类

--- statement.js
+++ statement.js
@@ -2,17 +2,21 @@ const invoices = require('./invoices.js')
 const plays = require('./plays.js')
 const createStatementData = require('./createStatementData.js')
 
-function statement (invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays));
+function htmlStatement (invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
 }
 
-function renderPlainText (data) {
-  let result = `Statement for ${data.customer}\n`;
+function renderHtml (data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
-  result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
   console.log(result)
   return result;
 }
@@ -25,4 +29,4 @@ function usd (aNumber) {
     }).format(aNumber / 100);
 }
 
-statement(invoices[0], plays)
+htmlStatement(invoices[0], plays)
